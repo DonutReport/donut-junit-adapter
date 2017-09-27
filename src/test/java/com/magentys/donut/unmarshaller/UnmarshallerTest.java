0aa2eb9487e33839e3c5e71557d6fda1310a4223
Amit Sharma <amit.sharma@magentys.io>
2017-09-27 14:35:39 +0800

    Modified two tests.

--- src/test/java/com/magentys/donut/unmarshaller/UnmarshallerTest.java
+++ src/test/java/com/magentys/donut/unmarshaller/UnmarshallerTest.java
@@ -8,12 +8,10 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
-
 public class UnmarshallerTest {
 
     private static final String MULTIPLE_RESULTS_FOLDER_ABS = FileUtils.toFile(UnmarshallerTest.class.getResource("/junit/multiple_results")).getAbsolutePath();
-    private static final String MULTIPLE_RESULTS_FOLDER_REL = "junit/multiple_results";
+    private static final String MULTIPLE_RESULTS_FOLDER_REL = "src/test/resources/junit/multiple_results";
     private final String sample1Path = FileUtils.toFile(UnmarshallerTest.class.getResource("/junit/single_result/TestResult.xml")).getAbsolutePath();
     private final XmlUtils xmlUtils = new XmlUtils();
 
@@ -25,24 +23,14 @@ public class UnmarshallerTest {
 
     @Test
     public void shouldReturnTestsuitesWhenAFolderAbsolutePathIsProvided() throws Exception {
-        Object object = xmlUtils.unmarshal(MULTIPLE_RESULTS_FOLDER_ABS);
-
-        Assert.assertTrue(object instanceof List);
-
-        List<Testsuite> testsuites = (List<Testsuite>) object;
-        Assert.assertTrue(testsuites.size() == 2);
+        List<Testsuite> testSuites = xmlUtils.unmarshal(MULTIPLE_RESULTS_FOLDER_ABS);
+        Assert.assertTrue(testSuites.size() == 2);
     }
 
     @Test
-    public void shouldThrowAnExceptionWhenProvidedARelativePathToResultFolder() throws Exception {
-        try {
-
-            xmlUtils.unmarshal(MULTIPLE_RESULTS_FOLDER_REL);
-
-        } catch (Exception e) {
-            assertTrue("", e.getMessage().contains("Provided path isn't a directory or the path isn't absolute."));
-        }
-
+    public void shouldReturnTestsuitesWhenProvidedARelativePathToResultFolder() throws Exception {
+        List<Testsuite> testSuites = xmlUtils.unmarshal(MULTIPLE_RESULTS_FOLDER_REL);
+        Assert.assertTrue(testSuites.size() == 2);
     }
 
 }
