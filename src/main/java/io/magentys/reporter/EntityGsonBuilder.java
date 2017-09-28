6d9444b1a30a70d5aded21b01d3d37d7b2230e6a
Amit Sharma <amit.sharma@magentys.io>
2017-09-28 10:55:24 +0800

    Refactored the package to io.magentys instead of io.magentys.donut

---
+++ src/main/java/io/magentys/reporter/EntityGsonBuilder.java
@@ -0,0 +1,12 @@
+package io.magentys.reporter;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+class EntityGsonBuilder {
+    static Gson createGson() {
+        return new GsonBuilder().setPrettyPrinting()
+                .create();
+    }
+}
---
+++ src/test/java/io/magentys/unmarshaller/UnmarshallerTest.java
@@ -0,0 +1,36 @@
+package io.magentys.unmarshaller;
+
+import io.magentys.jaxb.XmlUtils;
+import io.magentys.junit.model.Testsuite;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UnmarshallerTest {
+
+    private static final String MULTIPLE_RESULTS_FOLDER_ABS = FileUtils.toFile(UnmarshallerTest.class.getResource("/junit/multiple_results")).getAbsolutePath();
+    private static final String MULTIPLE_RESULTS_FOLDER_REL = "src/test/resources/junit/multiple_results";
+    private final String sample1Path = FileUtils.toFile(UnmarshallerTest.class.getResource("/junit/single_result/TestResult.xml")).getAbsolutePath();
+    private final XmlUtils xmlUtils = new XmlUtils();
+
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionWhenFilePathIsProvided() throws Exception {
+        xmlUtils.unmarshal(sample1Path);
+    }
+
+    @Test
+    public void shouldReturnTestsuitesWhenAFolderAbsolutePathIsProvided() throws Exception {
+        List<Testsuite> testSuites = xmlUtils.unmarshal(MULTIPLE_RESULTS_FOLDER_ABS);
+        Assert.assertTrue(testSuites.size() == 2);
+    }
+
+    @Test
+    public void shouldReturnTestsuitesWhenProvidedARelativePathToResultFolder() throws Exception {
+        List<Testsuite> testSuites = xmlUtils.unmarshal(MULTIPLE_RESULTS_FOLDER_REL);
+        Assert.assertTrue(testSuites.size() == 2);
+    }
+
+}
